5e196419381241dd577e22110c4a6990cf51889d
vincentiusvin <vincentius_vin@yahoo.com>
2023-12-03 20:46:53 +0700

    Deleted unecesary testing code

--- app/src/main/java/com/example/graiddle/MainActivity.java
+++ app/src/main/java/com/example/graiddle/MainActivity.java
@@ -21,24 +21,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        try{
-            Tasks.await(auth.signInWithEmailAndPassword("vincentiusvin1@gmail.com", "abcdef"));
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
-        User newUser = new User(1, "Udin", "123");
-        newUser.push(task -> {
-            if (task.isSuccessful()) {
-                Toast.makeText(MainActivity.this, "done", Toast.LENGTH_SHORT).show();
-            } else {
-                Exception e = task.getException();
-                if (e != null) {
-                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
     }
 }
