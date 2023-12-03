4d040a64faa48823d725261f1e3ca63b381ef9cc
vincentiusvin <vincentius_vin@yahoo.com>
2023-12-03 22:21:50 +0700

    Recyclerview done

--- app/src/main/java/com/example/graiddle/MainActivity.java
+++ app/src/main/java/com/example/graiddle/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.graiddle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -8,17 +11,42 @@ import android.widget.Toast;
 import com.example.graiddle.models.Recipe;
 import com.example.graiddle.models.User;
 import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private String EMAIL = "vincentiusvin1@gmail.com";
+    private String PASSWORD = "abcdef";
+
+    RecyclerView rvRecipes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+            Tasks.await(auth.signInWithEmailAndPassword(EMAIL, PASSWORD));
+        }catch (Exception e){}
+
+        rvRecipes = findViewById(R.id.recipesRV);
+
+        Recipe.getCollection().addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                List<Recipe> result = value.toObjects(Recipe.class);
+                rvRecipes.setAdapter(new RecipeListAdapter(result));
+                rvRecipes.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/graiddle/RecipeListAdapter.java
@@ -0,0 +1,52 @@
+package com.example.graiddle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.graiddle.models.Recipe;
+
+import java.util.List;
+
+public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.RecipeListViewHolder> {
+    private List<Recipe> items;
+
+    public RecipeListAdapter(List<Recipe> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public RecipeListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View v = inflater.inflate(R.layout.item_recipecard, parent, false);
+        return new RecipeListViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecipeListViewHolder holder, int position) {
+        Recipe item = items.get(position);
+        holder.tvName.setText(item.getName());
+        holder.tvDesc.setText(item.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    class RecipeListViewHolder extends RecyclerView.ViewHolder{
+        TextView tvName;
+        TextView tvDesc;
+
+        public RecipeListViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvName = itemView.findViewById(R.id.nameTV);
+            tvDesc = itemView.findViewById(R.id.descTV);
+        }
+    }
+}
