12b8ad887555021329cceb5d84b1393b90c287ae
vincentiusvin <vincentius_vin@yahoo.com>
2023-12-03 21:40:55 +0700

    More tests

--- app/src/androidTest/java/com/example/graiddle/FirebaseTests.java
+++ app/src/androidTest/java/com/example/graiddle/FirebaseTests.java
@@ -1,12 +1,19 @@
 package com.example.graiddle;
 
+import androidx.annotation.Nullable;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.example.graiddle.models.Recipe;
 import com.example.graiddle.models.User;
+import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,7 +21,10 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
+import android.util.Log;
+
 import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(AndroidJUnit4.class)
 public class FirebaseTests {
@@ -59,5 +69,30 @@ public class FirebaseTests {
     @Test
     public void addRecipe() throws Exception{
         Tasks.await(recipe.push());
+    }
+
+    @Test
+    public void viewRecipe(){
+        ArrayList<Recipe> recipes = new ArrayList<Recipe>();
+        Recipe.getCollection().addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                for(QueryDocumentSnapshot doc : value){
+                    long id = doc.getLong("id");
+                    String name = doc.getString("name");
+                    String description = doc.getString("description");
+                    List<String> ingredients = (List<String>) doc.get("ingredients");
+                    List<String> steps = (List<String>) doc.get("steps");
+                    DocumentReference creator = doc.getDocumentReference("creator"); // references users
+                    recipes.add(new Recipe(id, name, description, ingredients, steps, creator));
+                }
+            }
+        });
+        while (recipes.size() == 0){
+            try{
+                wait(1000);
+            }catch (Exception e){}
+        }
+        assertEquals(1, recipes.size());
     }
 }
