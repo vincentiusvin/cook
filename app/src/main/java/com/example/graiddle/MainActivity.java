b3fb64b691c85f7ae732481d48537b73f3acc841
vincentiusvin <vincentius_vin@yahoo.com>
2023-12-03 17:27:25 +0700

    Firebase hello world

--- app/src/main/java/com/example/graiddle/MainActivity.java
+++ app/src/main/java/com/example/graiddle/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, String> test = new HashMap<String, String>();
+        test.put("hello", "world");
+        db.collection("test").add(test);
     }
 }
