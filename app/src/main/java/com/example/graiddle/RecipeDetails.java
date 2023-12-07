805cfe56baa011d4fb70797b3b3ce786b2bd9a3c
vincentiusvin <vincentius_vin@yahoo.com>
2023-12-07 16:40:12 +0700

    Halo jus

---
+++ app/src/main/java/com/example/graiddle/RecipeDetails.java
@@ -0,0 +1,5 @@
+package com.example.graiddle;
+
+public class RecipeDetails {
+
+}
--- app/src/main/java/com/example/graiddle/RecipeListAdapter.java
+++ app/src/main/java/com/example/graiddle/RecipeListAdapter.java
@@ -1,9 +1,11 @@
 package com.example.graiddle;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -32,6 +34,10 @@ public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.Re
         Recipe item = items.get(position);
         holder.tvName.setText(item.getDisplayName());
         holder.tvDesc.setText(item.getDescription());
+//        holder.root.setOnClickListener(e -> {
+//            Intent openDetail = new Intent(holder.root.getContext(), RecipeDetails.class);
+//            holder.root.getContext().startActivity(openDetail);
+//        });
     }
 
     @Override
@@ -42,9 +48,11 @@ public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.Re
     class RecipeListViewHolder extends RecyclerView.ViewHolder{
         TextView tvName;
         TextView tvDesc;
+        View root;
 
         public RecipeListViewHolder(@NonNull View itemView) {
             super(itemView);
+            root = itemView;
             tvName = itemView.findViewById(R.id.nameTV);
             tvDesc = itemView.findViewById(R.id.descTV);
         }
