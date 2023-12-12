81684371c30dff48f61a4c0e62faa9482e1bf516
vincentiusvin <vincentius_vin@yahoo.com>
2023-12-12 14:57:28 +0700

    Cleanup

--- app/src/main/java/com/example/graiddle/HomeActivity.java
+++ app/src/main/java/com/example/graiddle/HomeActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.graiddle.models.Recipe;
 import com.example.graiddle.utils.HomeAdapter;
@@ -18,37 +20,32 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
-    private String EMAIL = "testing@mail.com";
-    private String PASSWORD = "123456";
-
     private RecyclerView rvRecipes;
-
     private HomeAdapter adapter;
+    Button btnAdd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        startActivity(new Intent(this, AddRecipeActivity.class));
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         rvRecipes = findViewById(R.id.menuRV);
+        btnAdd = findViewById(R.id.addBtn);
 
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
         rvRecipes.setLayoutManager(gridLayoutManager);
         rvRecipes.setHasFixedSize(true);
 
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        auth.signInWithEmailAndPassword(EMAIL, PASSWORD).addOnSuccessListener(l -> {
-            Recipe.getCollection().addSnapshotListener(new EventListener<QuerySnapshot>() {
-                @Override
-                public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                    List<Recipe> result = value.toObjects(Recipe.class);
-                    adapter = new HomeAdapter(HomeActivity.this, result);
-                    rvRecipes.setAdapter(adapter);
-                }
-            });
+        btnAdd.setOnClickListener(v -> {
+            startActivity(new Intent(HomeActivity.this, AddRecipeActivity.class));
         });
 
+        Recipe.getCollection().addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                List<Recipe> result = value.toObjects(Recipe.class);
+                adapter = new HomeAdapter(HomeActivity.this, result);
+                rvRecipes.setAdapter(adapter);
+            }
+        });
     }
 }
