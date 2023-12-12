7cedf6aecfe105764f715f0413e39d9f0fc30f84
vincentiusvin <vincentius_vin@yahoo.com>
2023-12-12 09:34:56 +0700

    Image upload

--- app/src/main/java/com/example/graiddle/AddRecipeActivity.java
+++ app/src/main/java/com/example/graiddle/AddRecipeActivity.java
@@ -1,14 +1,22 @@
 package com.example.graiddle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.graiddle.models.Recipe;
@@ -16,12 +24,19 @@ import com.example.graiddle.models.User;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class AddRecipeActivity extends AppCompatActivity {
     Button btnAddIngs, btnAddSteps, btnResepPage;
     EditText etAddTitle, etAddDesc;
+    ImageView ivAddImage;
 
     RecyclerView rvAddIngs, rvAddSteps;
 
@@ -42,6 +57,7 @@ public class AddRecipeActivity extends AppCompatActivity {
         etAddDesc = findViewById(R.id.addDescET);
         rvAddIngs = findViewById(R.id.addIngsRV);
         rvAddSteps = findViewById(R.id.addStepsRV);
+        ivAddImage = findViewById(R.id.addImageIV);
 
         ings = new ArrayList<String>();
         ings.add("Ingredient 1");
@@ -70,13 +86,42 @@ public class AddRecipeActivity extends AppCompatActivity {
         btnResepPage.setOnClickListener(v -> {
             String displayName = String.valueOf(etAddTitle.getText());
             String desc = String.valueOf(etAddDesc.getText());
+            String imgID = UUID.randomUUID().toString() + ".jpeg";
+
             DocumentReference user = User.refById(auth.getUid());
-            new Recipe(displayName, desc, ings, steps, user, "bakso.jpeg")
+            new Recipe(displayName, desc, ings, steps, user, imgID)
                 .push().addOnSuccessListener(u -> {
                     Toast.makeText(AddRecipeActivity.this, "Item added successfully!", Toast.LENGTH_SHORT).show();
                 }).addOnFailureListener(u -> {
                     Toast.makeText(AddRecipeActivity.this, "Item was not added!", Toast.LENGTH_SHORT).show();
                 });
+
+            Bitmap bmImg = ((BitmapDrawable) ivAddImage.getDrawable()).getBitmap();
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            bmImg.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+            byte[] bytes = bos.toByteArray();
+
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference image = storage.getReference().child(imgID);
+            StorageTask storageTask = image.putBytes(bytes)
+                .addOnSuccessListener(t -> {
+                    Toast.makeText(this, "Image added succesfully!", Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(t -> {
+                    Toast.makeText(this, "Image was not added!", Toast.LENGTH_SHORT).show();
+                });
         });
+
+    ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
+        registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
+            ivAddImage.setImageURI(uri);
+        });
+
+    ivAddImage.setOnClickListener(v -> {
+        pickMedia.launch(new PickVisualMediaRequest.Builder() // errornya diignore aja
+            .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
+            .build());
+    });
+
     }
 }
--- app/src/main/java/com/example/graiddle/MainActivity.java
+++ app/src/main/java/com/example/graiddle/MainActivity.java
@@ -39,40 +39,30 @@ public class MainActivity extends AppCompatActivity {
     private HomeAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        startActivity(new Intent(this, AddRecipeActivity.class));
+//        startActivity(new Intent(this, AddRecipeActivity.class));
 
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mRecyclerView = findViewById(R.id.menuRV);
-        titles = new ArrayList<>();
-        mImages = new ArrayList<>();
-        adapter = new HomeAdapter(this, titles, mImages);
-        mImages.add("bakso.jpeg");
-        mImages.add("mie.jpg");
-
-        titles.add("Bakso Malang");
-        titles.add("Mie Goreng");
-
 
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
         mRecyclerView.setLayoutManager(gridLayoutManager);
         mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.setAdapter(adapter);
 
         try {
             FirebaseAuth auth = FirebaseAuth.getInstance();
             Tasks.await(auth.signInWithEmailAndPassword(EMAIL, PASSWORD));
         }catch (Exception e){}
 
-//        Recipe.getCollection().addSnapshotListener(new EventListener<QuerySnapshot>() {
-//            @Override
-//            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-//                List<Recipe> result = value.toObjects(Recipe.class);
-//                rvRecipes.setAdapter(new RecipeListAdapter(result));
-//                rvRecipes.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-//            }
-//        });
+        Recipe.getCollection().addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                List<Recipe> result = value.toObjects(Recipe.class);
+                adapter = new HomeAdapter(MainActivity.this, result);
+                mRecyclerView.setAdapter(adapter);
+            }
+        });
     }
 }
