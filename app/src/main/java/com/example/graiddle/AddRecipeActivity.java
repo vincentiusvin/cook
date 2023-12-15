3fda356a0f4efb255717b82c54c098367369eaa4
Shan Havilah <shanhavilah21@gmail.com>
2023-12-15 16:30:15 +0700

    navbar di home + back button di add recipe

--- app/src/main/java/com/example/graiddle/AddRecipeActivity.java
+++ app/src/main/java/com/example/graiddle/AddRecipeActivity.java
@@ -30,7 +30,7 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public class AddRecipeActivity extends AppCompatActivity {
-    Button btnAddIngs, btnAddSteps, btnResepPage;
+    Button btnAddIngs, btnAddSteps, btnResepPage, btnBack;
     EditText etAddTitle, etAddDesc;
     ImageView ivAddImage;
 
@@ -54,6 +54,12 @@ public class AddRecipeActivity extends AppCompatActivity {
         rvAddIngs = findViewById(R.id.addIngsRV);
         rvAddSteps = findViewById(R.id.addStepsRV);
         ivAddImage = findViewById(R.id.addImageIV);
+        btnBack = findViewById(R.id.backButton);
+
+        btnBack.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+            finish();
+        });
 
         ings = new ArrayList<String>();
         ings.add("Ingredient 1");
--- app/src/main/java/com/example/graiddle/HomeActivity.java
+++ app/src/main/java/com/example/graiddle/HomeActivity.java
@@ -8,10 +8,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.graiddle.models.Recipe;
 import com.example.graiddle.utils.HomeAdapter;
 import com.google.android.gms.tasks.Tasks;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestoreException;
@@ -22,22 +24,22 @@ import java.util.List;
 public class HomeActivity extends AppCompatActivity {
     private RecyclerView rvRecipes;
     private HomeAdapter adapter;
-    Button btnAdd;
+    //Button btnAdd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         rvRecipes = findViewById(R.id.menuRV);
-        btnAdd = findViewById(R.id.addBtn);
+        //btnAdd = findViewById(R.id.addBtn);
 
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
         rvRecipes.setLayoutManager(gridLayoutManager);
         rvRecipes.setHasFixedSize(true);
 
-        btnAdd.setOnClickListener(v -> {
-            startActivity(new Intent(HomeActivity.this, AddRecipeActivity.class));
-        });
+//        btnAdd.setOnClickListener(v -> {
+//            startActivity(new Intent(HomeActivity.this, AddRecipeActivity.class));
+//        });
 
         Recipe.getCollection().addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
@@ -46,6 +48,27 @@ public class HomeActivity extends AppCompatActivity {
                 adapter = new HomeAdapter(HomeActivity.this, result);
                 rvRecipes.setAdapter(adapter);
             }
+        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.home_nav);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if(item.getItemId()==R.id.home_nav){
+                return true;
+            }
+            else if(item.getItemId()==R.id.add_nav){
+                startActivity(new Intent(getApplicationContext(), AddRecipeActivity.class));
+                finish();
+                return true;
+            }
+            else if(item.getItemId()==R.id.logout_nav){
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                Toast.makeText(getApplicationContext(), "Log out account", Toast.LENGTH_SHORT).show();
+                finish();
+                return true;
+            }
+            return false;
         });
     }
 }
